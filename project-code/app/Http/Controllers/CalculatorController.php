465dc068aafe8c3022b39b020fca605f693028b0
root <root@master.localdomain>
2024-12-28 19:33:53 +0100

    initial commit 3

--- project-code/app/Http/Controllers/CalculatorController.php
+++ project-code/app/Http/Controllers/CalculatorController.php
@@ -3,17 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Calculation; // Import the Calculation model
 
 class CalculatorController extends Controller
 {
+    // Show the calculator form
     public function index()
     {
         return view('calculator');
     }
 
+    // Perform the calculation and save the result to the database
     public function calculate(Request $request)
     {
         $result = 0;
+
+        // Perform the calculation based on the selected operation
         switch ($request->operation) {
             case 'add':
                 $result = $request->num1 + $request->num2;
@@ -29,6 +34,12 @@ class CalculatorController extends Controller
                 break;
         }
 
+        // Save the result to the database using the Calculation model
+        $calculation = new Calculation();
+        $calculation->result = $result;
+        $calculation->save(); // Insert the calculation result into the database
+
+        // Return the result to the view
         return view('calculator', compact('result'));
     }
 }
